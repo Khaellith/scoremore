2ca83de788bcc8f90ff0184c57e7d5037ddb8044
Nieki <1452657houtvast@zuyd.nl>
2015-04-12 13:48:21 +0200

    tentamen starten

--- scoremore/MainActivity.cs
+++ scoremore/MainActivity.cs
@@ -12,7 +12,6 @@ namespace scoremore
 	[Activity (Label = "scoremore", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,13 +20,30 @@ namespace scoremore
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
+			Spinner spinnerOnderwerp = FindViewById<Spinner> (Resource.Id.spinner1);
+
+			spinnerOnderwerp.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinnerOnderwerp_ItemSelected);
+			var adapter = ArrayAdapter.CreateFromResource (
+				this, Resource.Array.onderwerp_array, Android.Resource.Layout.SimpleSpinnerItem);
+
+			adapter.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
+			spinnerOnderwerp.Adapter = adapter;
+
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button ButtonTentamenMaken = FindViewById<Button> (Resource.Id.myButton);
 			
 			ButtonTentamenMaken.Click += delegate {
 				SetContentView (Resource.Layout.TentamenStarten);
-			};
+			};				
+		}
+
+		private void spinnerOnderwerp_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+		{
+			Spinner spinnerOnderwerp = (Spinner)sender;
+
+			string toast = string.Format ("Het onderwerp is {0}", spinnerOnderwerp.GetItemAtPosition (e.Position));
+
 		}
 	}
 }
--- scoremore/obj/Debug/android/scoremore/scoremore/R.java
+++ scoremore/obj/Debug/android/scoremore/scoremore/R.java
@@ -8,31 +8,33 @@
 package scoremore.scoremore;
 
 public final class R {
+    public static final class array {
+        public static final int onderwerp_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int button2=0x7f050001;
-        public static final int button3=0x7f050003;
-        public static final int button4=0x7f050004;
-        public static final int myButton=0x7f050000;
-        public static final int radioButton2=0x7f050009;
-        public static final int radioButton3=0x7f05000a;
-        public static final int radioButton4=0x7f05000b;
-        public static final int radioGroup1=0x7f050005;
-        public static final int spinner1=0x7f050007;
-        public static final int textView1=0x7f050008;
-        public static final int textView2=0x7f050006;
+        public static final int button1=0x7f060009;
+        public static final int button2=0x7f060001;
+        public static final int myButton=0x7f060000;
+        public static final int radioButton2=0x7f060006;
+        public static final int radioButton3=0x7f060007;
+        public static final int radioButton4=0x7f060008;
+        public static final int radioGroup1=0x7f060004;
+        public static final int spinner1=0x7f060003;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int tentamenstarten=0x7f030001;
+        public static final int vraagbeantwoorden=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int onderwerp_prompt=0x7f040001;
     }
 }
