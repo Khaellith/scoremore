fc18ae5ac7e844d10f7be7802c209675f56c4026
Nieki <1452657houtvast@zuyd.nl>
2015-04-13 18:18:16 +0200

    spinner

--- scoremore/MainActivity.cs
+++ scoremore/MainActivity.cs
@@ -20,32 +20,49 @@ namespace scoremore
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			Spinner spinnerOnderwerp = FindViewById<Spinner> (Resource.Id.spinner1);
-
-//			spinnerOnderwerp.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinnerOnderwerp_ItemSelected);
-			var adapter = ArrayAdapter.CreateFromResource (
-				this, Resource.Array.onderwerp_array, Android.Resource.Layout.SimpleSpinnerItem);
-
-			adapter.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
-//			spinnerOnderwerp.Adapter = adapter;
-
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button ButtonTentamenMaken = FindViewById<Button> (Resource.Id.myButton);
 			
 			ButtonTentamenMaken.Click += delegate {
 				SetContentView (Resource.Layout.TentamenStarten);
+
+				Spinner spinnerOnderwerp = FindViewById<Spinner> (Resource.Id.spinner);
+
+				spinnerOnderwerp.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinner_ItemSelected);
+				var adapter = ArrayAdapter.CreateFromResource (
+					this, Resource.Array.onderwerp_array, Android.Resource.Layout.SimpleSpinnerItem);
+
+				adapter.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
+				spinnerOnderwerp.Adapter = adapter;
 			};				
 		}
 
-		private void spinnerOnderwerp_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+		private void spinner_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
 		{
-			Spinner spinnerOnderwerp = (Spinner)sender;
+			Spinner spinner = (Spinner)sender;
+			string toast = string.Format ("Het gekozen onderwerp is {0}", spinner.GetItemAtPosition (e.Position));
 
-			string toast = string.Format ("Het onderwerp is {0}", spinnerOnderwerp.GetItemAtPosition (e.Position));
+			if (e.Position == 1) {
+				string test = "Hallo";
+				Toast.MakeText (this, test, ToastLength.Long).Show ();
+				Spinner spinnerSubonderwerp = FindViewById<Spinner> (Resource.Id.spinner1);
 
+				spinnerSubonderwerp.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinner_ItemSelected);
+				var adapter1 = ArrayAdapter.CreateFromResource (
+					               this, Resource.Array.subonderwerpWiskunde_array, Android.Resource.Layout.SimpleSpinnerItem);
+
+				adapter1.SetDropDownViewResource (Android.Resource.Layout.SimpleSpinnerDropDownItem);
+				spinnerSubonderwerp.Adapter = adapter1;
+
+				spinner_ItemSelected (spinnerSubonderwerp, AdapterView.ItemSelectedEventArgs e));
+		
+
+			}
 		}
+			
 	}
 }
+
 
 
--- scoremore/obj/Debug/android/src/scoremore/scoremore/R.java
+++ scoremore/obj/Debug/android/src/scoremore/scoremore/R.java
@@ -10,6 +10,7 @@ package scoremore.scoremore;
 public final class R {
     public static final class array {
         public static final int onderwerp_array=0x7f050000;
+        public static final int subonderwerpWiskunde_array=0x7f050001;
     }
     public static final class attr {
     }
@@ -17,15 +18,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f06000a;
+        public static final int button1=0x7f060007;
         public static final int button2=0x7f060001;
         public static final int listView1=0x7f060003;
         public static final int myButton=0x7f060000;
-        public static final int radioButton2=0x7f060007;
-        public static final int radioButton3=0x7f060008;
-        public static final int radioButton4=0x7f060009;
-        public static final int radioGroup1=0x7f060005;
-        public static final int spinner1=0x7f060004;
+        public static final int spinner=0x7f060004;
+        public static final int spinner1=0x7f060005;
         public static final int textView1=0x7f060002;
         public static final int textView2=0x7f060006;
     }
@@ -37,7 +35,7 @@ public final class R {
         public static final int vragendownloaden=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int onderwerp_prompt=0x7f040001;
+        public static final int onderwerp_prompt=0x7f040000;
+        public static final int subonderwerp_prompt=0x7f040001;
     }
 }
