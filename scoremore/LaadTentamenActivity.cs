ee833fcee8f5b6fa0780f237a166b4d4de8596a0
Nieki <1452657houtvast@zuyd.nl>
2015-04-13 15:57:59 +0200

    sync

---
+++ scoremore/LaadTentamenActivity.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android;
+using scoremore;
+
+namespace scoremore
+{
+	[Activity (Label = "LaadTentamenActivity")]			
+	public class LaadTentamenActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			//base.OnCreate (bundle);
+	
+			//Spinner spinnerOnderwerp = new Spinner ();
+
+			List<String> SpinnerInhoud = new List<String> ();
+
+			foreach (KeyValuePair<String, List<KeyValuePair<String, List<Vraag>>>> onderwerpKVP in Singleton.LocalDatabase) {
+				foreach (KeyValuePair<String, List<Vraag>> subonderwerpKVP in onderwerpKVP.Value) {
+					SpinnerInhoud.Add (onderwerpKVP.Key + ": " + subonderwerpKVP.Key);
+				}}
+			
+
+
+		}
+			
+	}
+}
+
